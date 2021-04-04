3d4975f402cd9d290bb9ff44e808aa4f02493b7d
Sanusiharuna024 <81807406+Sanusiharuna024@users.noreply.github.com>
2021-04-04 15:42:01 +0100

    Add files via upload

---
+++ Sanusi haruna (b) .py
@@ -0,0 +1,25 @@
+print("question one")
+print('name:sanusi haruna')
+print("department:computer science")
+print("question two")
+scare=('boo!')
+print(scare)
+print('question three')
+boza1=3
+boza2=5
+boza3=boza1+boza2
+print(boza1,boza2,boza3)
+print("question four")
+result=12/.5
+print(result)
+print("question five")
+boza=15+20
+print(boza)
+print("question six")
+boza=8%3
+print(boza)
+print("question seven")
+result1=2*4
+result2=4+2
+result3=result1*result2
+print(result3)
